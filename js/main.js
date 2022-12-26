0f42bf20986b525f2d661315c1d57c80a6ad50c8
Temur Zhakibaev <temurzhakibaev@gmail.com>
2022-12-26 13:28:46 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const $block = document.querySelector("#wrapper");
+const $item = document.querySelector("#item");
+const $btn = document.querySelector("#btn");
+console.log($btn);
+console.log($block);
+
+$btn.addEventListener("click", () => {});
